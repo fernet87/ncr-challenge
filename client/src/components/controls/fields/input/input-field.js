98617391c245fccf766bc0c81cfcc20b7b774970
Fernando Rouco <rouco.fernando@gmail.com>
2021-09-01 18:00:28 -0300

    Fix input field warning

--- client/src/components/controls/fields/input/input-field.js
+++ client/src/components/controls/fields/input/input-field.js
@@ -78,7 +78,7 @@ export default function InputField(props) {
     
   useEffect(() => {
     setValue(props.attr, model.get(props.attr));
-  }, []);
+  }, [setValue, props.attr, model]);
 
   useEffect(() => {
     handleErrorClasses();
