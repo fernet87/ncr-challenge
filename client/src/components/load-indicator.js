91cb77173f0fd9da6ef9d400ae6ce3759e25c5f2
Fernando Rouco <rouco.fernando@gmail.com>
2021-09-06 18:01:16 -0300

    Minor fix in indicator

--- client/src/components/load-indicator.js
+++ client/src/components/load-indicator.js
@@ -13,7 +13,7 @@ const StyledLoadingContainer = styled.div`
 const isEmpty = (prop) => (
   prop === null ||
   (prop !== undefined &&
-    ((prop.hasOwnProperty('length') && prop.length === 0) || (prop.constructor === Object && Object.keys(prop).length === 0)))
+    ((prop.constructor === Object && Object.keys(prop).length === 0)))
 );
  
 const withLoader = (loadingProp) => (WrappedComponent) => {
