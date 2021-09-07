fa5ac19663fc08c1f408719150db55ce1d92860a
Fernando Rouco <rouco.fernando@gmail.com>
2021-09-07 16:06:53 -0300

    Tooltip Fix

--- client/src/components/tooltip.js
+++ client/src/components/tooltip.js
@@ -6,62 +6,64 @@ export default function Tooltip(props) {
   const tooltipRef = createRef();
 
   useEffect(() => {
-    const tooltip = tooltipRef.current;
     const popcorn = document.querySelectorAll('[data-toggle="tooltip"]')[0];
     
-    const popperInstance = createPopper(popcorn, tooltip, {
-      placement: 'left',
-      modifiers: [
-        {
-          name: 'offset',
-          options: {
-            offset: [0, 8],
+    if (popcorn) {
+      const tooltip = tooltipRef.current;
+      const popperInstance = createPopper(popcorn, tooltip, {
+        placement: 'left',
+        modifiers: [
+          {
+            name: 'offset',
+            options: {
+              offset: [0, 8],
+            },
           },
-        },
-      ]
-    });
-    
-    function show() {
-      // Make the tooltip visible
-      tooltip.setAttribute('data-show', '');
-    
-      // Enable the event listeners
-      popperInstance.setOptions((options) => ({
-        ...options,
-        modifiers: [
-          ...options.modifiers,
-          { name: 'eventListeners', enabled: true }
-        ],
-      }));
-    
-      // Update its position
-      popperInstance.update();
+        ]
+      });
+      
+      function show() {
+        // Make the tooltip visible
+        tooltip.setAttribute('data-show', '');
+      
+        // Enable the event listeners
+        popperInstance.setOptions((options) => ({
+          ...options,
+          modifiers: [
+            ...options.modifiers,
+            { name: 'eventListeners', enabled: true }
+          ],
+        }));
+      
+        // Update its position
+        popperInstance.update();
+      }
+      
+      function hide() {
+        // Hide the tooltip
+        tooltip.removeAttribute('data-show');
+      
+        // Disable the event listeners
+        popperInstance.setOptions((options) => ({
+          ...options,
+          modifiers: [
+            ...options.modifiers,
+            { name: 'eventListeners', enabled: false }
+          ],
+        }));
+      }
+
+      const showEvents = ['mouseenter', 'focus'];
+      const hideEvents = ['mouseleave', 'blur'];     
+
+      showEvents.forEach((event) => {
+        popcorn.addEventListener(event, show);
+      });
+      
+      hideEvents.forEach((event) => {
+        popcorn.addEventListener(event, hide);
+      });
     }
-    
-    function hide() {
-      // Hide the tooltip
-      tooltip.removeAttribute('data-show');
-    
-      // Disable the event listeners
-      popperInstance.setOptions((options) => ({
-        ...options,
-        modifiers: [
-          ...options.modifiers,
-          { name: 'eventListeners', enabled: false }
-        ],
-      }));
-    }
-    
-    const showEvents = ['mouseenter', 'focus'];
-    const hideEvents = ['mouseleave', 'blur'];
-    
-    showEvents.forEach((event) => {
-      popcorn.addEventListener(event, show);
-    });
-    
-    hideEvents.forEach((event) => {
-      popcorn.addEventListener(event, hide);
-    });
   });
   
   return (
